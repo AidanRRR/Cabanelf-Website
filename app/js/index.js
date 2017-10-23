44581af2449289db5fe3a1cced974b3717df7a13
DESKTOP-8OUDKEN\Aidan <aidan.rypens@outlook.com>
2017-10-23 21:47:00 +0200

    Home pagina

---
+++ app/js/index.js
@@ -0,0 +1,15 @@
+$(".nav-pic-1").hover(
+    function () {
+        $("#cabanelf-main").toggleClass("cabanelf-main-hide");
+    }
+);
+$(".nav-pic-2").hover(
+    function () {
+        $("#cabanelf-main").toggleClass("cabanelf-main-hide");
+    }
+);
+$(".nav-pic-3").hover(
+    function () {
+        $("#cabanelf-main").toggleClass("cabanelf-main-hide");
+    }
+);
